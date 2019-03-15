b5d0cefcaa4a8cda7d41043824aea4ec68ab9ab2
ISMAEL MARTINEZ SEGUI <6001125@insags.com>
2019-03-15 11:08:32 +0100

    drag&Drop

--- src/main/webapp/resources/js/plataformas.js
+++ src/main/webapp/resources/js/plataformas.js
@@ -1,11 +1,36 @@
 //Con este codigo conseguimos que se mueva cada tarea unicamente en su eje x, y a su vez que cuando los dejes en el sitio, cambien de color
 $( function() {
-	$(".rect").draggable({ axis: "x" });
+
+	$(".rect").draggable(
+			{
+				axis: "x" , 
+				containment: ".table-responsive",
+				scroll: false,
+				opacity: 0.7,
+				helper: "clone",
+				appendTo: "td"
+			}
+	);
+
 	$( "td" ).droppable({
+
 		drop: function( event, ui ) {
 			// Pinta de rojo todos los modificados
-			$( event.originalEvent.target ).addClass( "red" );
+			$( ui.draggable[0] ).addClass( "orange" );
+			var $this = $(this);
+			ui.draggable.position({
+				my: "center",
+				at: "center",
+				of: $this,
+				using: function(pos) {
+					$(this).animate(pos, 200, "linear");
+				}
+			});
+			
+//			console.log($("tr")[$( ui.draggable)[0].attributes[2].value].children);
+//			console.log($("tr")[$( ui.draggable)[0].attributes[2].value].children[$( ui.draggable)[0].attributes[1].value].length);
+//			$(".rect").clone().attr({"class":"first"}).appendTo($("tr")[$( ui.draggable)[0].attributes[2].value].children[$( ui.draggable)[0].attributes[1].value]);
 		}
 	});
-//	$( "td" ).selectable();
+
 })
