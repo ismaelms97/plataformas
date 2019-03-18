c573f6983435c63b2ac3ab41199bc62c3c3d61b9
ISMAEL MARTINEZ SEGUI <6001125@insags.com>
2019-03-18 13:42:33 +0100

    drag&drop final touches

--- src/main/webapp/resources/js/plataformas.js
+++ src/main/webapp/resources/js/plataformas.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
 		var cln = $(el).clone();
 		cln.attr("class", "clone");
 		$(el).parent().append(cln);
-		console.log(cln);
 		$(cln).addClass("display");
 	}
 });
 
 //Con este codigo conseguimos que se mueva cada tarea unicamente en su eje x, y a su vez que cuando los dejes en el sitio, cambien de color
 $( function() {
+	var selected = $([]), offset = {top:0, left:0}; 
 	$(".rect").draggable(
 			{
 				axis: "x" , 
@@ -21,9 +21,36 @@ $( function() {
 				helper: "clone",
 				start: function(event, ui){
 					$(".ui-draggable-dragging").removeClass("noLeft");
+
+//					Descomentar esto para seleccionar
+//					if ($(this).hasClass("ui-selected")){
+//						selected = $(".ui-selected").each(function() {
+//							var el = $(this);
+//							el.data("offset", el.offset());
+//						});
+//					}
+//					else {
+//						selected = $([]);
+//						$(".rect").removeClass("ui-selected");
+//					}
+//					offset = $(this).offset();
+
 				},
+//				Descomentar esto para seleccionar
+//				drag: function(ev, ui) {
+//					var dt = ui.position.top - offset.top, dl = ui.position.left - offset.left;
+//					// Coje todos los elementos seleccionados excepto $(this), que es el elemento que queremos mover
+//					selected.not(this).each(function() {
+//
+//						// Crea la variable para que no necesitemos llamar a $(this)
+//						// el = Elemento actual
+//						// off = En que posicion esteaba este elemento antes de moverlo
+//						var el = $(this), off = el.data("offset");
+//						el.css({top: off.top + dt, left: off.left + dl});
+//					});
+//				},
+
 				stop: function(event, ui){
-//					$(".clone")[($(this).attr("data-rtc") - 1)].removeClass("display");
 					document.getElementsByClassName("clone")[(this.getAttribute("data-rtc") - 1)].classList.remove("display");
 				},
 			}
@@ -44,9 +71,35 @@ $( function() {
 				}
 			});
 
-			// Corregir, el rtc se mueve porque tiene un valor en el left que a la hora de hacer un append, le suma ese valor y parece que lo adjudique mal
 			$(ui.draggable[0]).addClass("noLeft").appendTo(event.target);
 		}
 	});
+
+	
+// SI se quiere seleccionar, solo hace falta desbloquear el siguiente codigo, aunque si es así, mirar la seleccion multiple
+
+//	$(".rect").selectable()
+
+	//Manualmente activa el select que los elementos clicados
+//	$( ".rect" ).click( function(e){
+//		if (e.ctrlKey == false) {
+//			// Si la tecla de comando se presiona no deselecciones los elementos
+//			$( ".rect" ).removeClass("ui-selected");
+//			$(this).addClass("ui-selecting");
+//		}
+//		else {
+//			if ($(this).hasClass("ui-selected")) {
+//				// Elimina la clase selected de lo elementos seleccionados
+//				$(this).removeClass("ui-selected");
+//			}
+//			else {
+//				// Sino, añadeles la clase
+//				$(this).addClass("ui-selecting");
+//			}
+//		}
+
+//		$( ".rect" ).data("selectable")._mouseStop(null);
+
+//	});
 
 })
