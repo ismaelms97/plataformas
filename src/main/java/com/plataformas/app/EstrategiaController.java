4c08d9b7f4772882adb8acb68787beffc3e83562
MARCOS YESTE MOLINA <6001123@insags.com>
2019-03-26 13:29:15 +0100

    EstrategiaController

---
+++ src/main/java/com/plataformas/app/EstrategiaController.java
@@ -0,0 +1,111 @@
+package com.plataformas.app;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.plataformas.Db2.EstrategiaService;
+import com.plataformas.model.Estrategia;
+import com.plataformas.model.Tarea;
+import com.plataformas.model.User;
+@Controller
+@RequestMapping(value = "/estrategia")
+public class EstrategiaController {
+
+
+	@Autowired
+	EstrategiaService estrategiaService;
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+
+
+	@GetMapping(value = "/panelControl")
+	public  String showEstrategia(Model model,HttpSession session) {	
+
+		synchronized (session) {
+			try {
+
+				User actualUser = (User) session.getAttribute("userSession");
+				List<Estrategia> listaEstrategias = estrategiaService.findEstrategiaById(actualUser.getEquipoId());	
+				model.addAttribute("listaEstrategia",listaEstrategias);
+				return "mainPanel";
+
+			}catch (Exception e) {
+				System.out.println("Panel de control Error con session o con estrategias");
+				return "redirect:/";
+			}
+
+		}
+
+	}
+	@GetMapping(value = "/findEstrategia/{id}")
+	public  String findEstrategia(@PathVariable String id,Model model,HttpSession session) {	
+		
+		synchronized (session) {
+			try {
+				List<Tarea> tareas = estrategiaService.findTareasByEstrategia(Integer.parseInt(id));
+				model.addAttribute("listaTareas",tareas);
+
+				System.out.println("TAREAS COMPLETE");
+				return "plataforma";
+			}catch (NumberFormatException e) {
+				System.out.println("formato incorrecto en mostrarTareasEstrategia Controller");
+				return "mainPanel";
+			}catch (Exception e) {
+				System.out.println("otra error en mostrarTareasEstrategia Controller");
+				return "mainPanel";
+			}		
+		}
+
+	}
+	@GetMapping(value = "/createEstrategia")
+	public String newEstrategia(Model model,HttpSession session) {
+
+		synchronized (session) {
+			User actualUser = (User) session.getAttribute("userSession");
+			model.addAttribute("estrategia", new Estrategia());
+			model.addAttribute("equipoId", actualUser.getEquipoId());
+		}
+
+		return "formularioEstrategia";
+	}
+
+	@PostMapping(value = "/saveEstrategia")
+	public  String saveEstrategia(@ModelAttribute("estrategia") Estrategia estrategia,Model model,HttpSession session) {	
+
+		synchronized (session) {
+			try {
+				estrategiaService.saveEstrategia(estrategia);
+
+			}catch (Exception e) {
+				System.out.println("error al guardar");
+			}
+
+			return "plataforma";
+		}
+
+	}
+
+	@PostMapping(value = "/updateEstrategia")
+	public String deleteEstrategia(Model model) {	
+
+
+		return "mainPanel";
+
+	}
+
+}
--- src/main/java/com/plataformas/app/HomeController.java
+++ src/main/java/com/plataformas/app/HomeController.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,9 +30,10 @@ import com.plataformas.model.User;
  * Handles requests for the application home page.
  */
 @Controller
+@RequestMapping(value = "/")
 public class HomeController {
 
-	private List<User> USessions = new ArrayList<User>();
+	private static  List<User> USessions = new ArrayList<User>();
 
 	@Autowired
 	UserService userService;
@@ -44,14 +47,15 @@ public class HomeController {
 	 * @throws SQLException 
 	 * @throws ClassNotFoundException 
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	@GetMapping(value = "/")
 	public String home(Locale locale, Model model) throws ClassNotFoundException, SQLException  {
 
 		model.addAttribute("user", new User());
 		return "home";
 	}
 
-	@RequestMapping(value = "/mainPanel", method = RequestMethod.POST)
+	@PostMapping(value = "/mainPanel")
 	public String login(@ModelAttribute("user") User user, Model model,HttpSession session){
 
 		User newUser = null;
@@ -101,109 +105,41 @@ public class HomeController {
 		}
 
 	}
-	
-	@RequestMapping(value = "/panelControl", method = RequestMethod.GET)
-	public String panelControl(Model model,HttpSession session){
-		
-		synchronized (session) {
-			try {
-				
-				User actualUser = (User) session.getAttribute("userSession");
-				List<Estrategia> listaEstrategias = estrategiaService.findEstrategiaById(actualUser.getEquipoId());	
-				model.addAttribute("listaEstrategia",listaEstrategias);
-				return "mainPanel";
-				
-			}catch (Exception e) {
-				System.out.println("Panel de control Error con session o con estrategias");
-				return "redirect:/";
-			}
-			
-		}
 
-
-	}
-
-	@RequestMapping(value = "/newEstrategia", method = RequestMethod.GET)
-	public String nuevaEstrategia( Model model,HttpSession session){
-		synchronized (session) {
-			User actualUser = (User) session.getAttribute("userSession");
-			model.addAttribute("estrategia", new Estrategia());
-			model.addAttribute("equipoId", actualUser.getEquipoId());
-		}
-		
-		
-
-		return "formularioEstrategia";
-
-
-	}
-	
-	@RequestMapping(value = "/newEstrategiaForm", method = RequestMethod.POST)
-	public String nuevaEstrategiaForm(@ModelAttribute("estrategia") Estrategia estrategia, Model model,HttpSession session){
-
-		try {
-			estrategiaService.saveEstrategia(estrategia);
-			
-		}catch (Exception e) {
-			System.out.println("error al guardar");
-		}
-
-		return "plataforma";
-
-
-	}
-	@RequestMapping(value = "/estrategia/{id}", method = RequestMethod.GET)
-	public String mostrarTareasEstrategia(@ModelAttribute("estrategia") Estrategia estrategia,@PathVariable String id , Model model,HttpSession session){		
-
-		try {
-			List<Tarea> tareas = estrategiaService.findTareasByEstrategia(Integer.parseInt(id));
-			model.addAttribute("listaTareas",tareas);
-
-			System.out.println("TAREAS COMPLETE");
-			return "plataforma";
-		}catch (NumberFormatException e) {
-			System.out.println("formato incorrecto en mostrarTareasEstrategia Controller");
-			return "mainPanel";
-		}catch (Exception e) {
-			System.out.println("otra error en mostrarTareasEstrategia Controller");
-			return "mainPanel";
-		}		
-
-	}
-
-	@RequestMapping(value = "/closeSession", method = RequestMethod.POST)
+	@PostMapping(value = "/closeSession")
 	public String SessionDestroy(HttpSession session) {	
-		
+		synchronized (session) {
 		session.invalidate();
-
+		}
 		return "redirect:/";
 
 	}
 	
-	@RequestMapping(value = "/saveStrategy", method = RequestMethod.GET)
-	public @ResponseBody String saveStrategy(Model model, String name, String startDate, String endDate, String team, String stratTasks) {	
+//	@GetMapping(value = "/saveStrategy")
+//	public @ResponseBody String saveStrategy(Model model, String name, String startDate, String endDate, String team, String stratTasks ) {	
+//		
+//		
+//		try {
+//			Estrategia estrategia = new Estrategia(name,"En curso",startDate,endDate,Integer.parseInt(team));
+//			estrategiaService.saveEstrategia(estrategia);
+//			
+//		}catch (Exception e) {
+//			System.out.println("error al guardar");
+//		}
+//		
+//		String[][] tasks = new String[stratTasks.split("qwer").length][4];
+//		
+//		for(int i = 0; i < tasks.length; i++) {
+//			String[] task = stratTasks.split("qwer");
+//			tasks[i] = task[i].split(",");
+//			System.out.println(tasks[i][0] + " - " + tasks[i][1]);
+//		}
+//		return "mainPanel";
+//		
+//	}
 	
-		
-		try {
-			Estrategia estrategia = new Estrategia(name,"En curso",startDate,endDate,Integer.parseInt(team));
-			estrategiaService.saveEstrategia(estrategia);
-			
-		}catch (Exception e) {
-			System.out.println("error al guardar");
-		}
-		
-		String[][] tasks = new String[stratTasks.split("qwer").length][4];
-		
-		for(int i = 0; i < tasks.length; i++) {
-			String[] task = stratTasks.split("qwer");
-			tasks[i] = task[i].split(",");
-			System.out.println(tasks[i][0] + " - " + tasks[i][1]);
-		}
-		return "mainPanel";
-	}
 	
-	
-	@RequestMapping(value = "/excel", method = RequestMethod.GET)
+	@GetMapping(value = "/excel")
 	public String login(Model model) {		
 
 		/* AQUI va el codigo para comprobar user en la base de datos. */
